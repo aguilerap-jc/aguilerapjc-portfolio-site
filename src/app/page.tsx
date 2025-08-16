f785b5eb2cc9e3edf88a4a5e1db3d272892fd3a0
aguilerap-jc <jcjc_1@hotmail.com>
2025-08-16 11:32:10 +0200

    cleanup: fix spacing in React import
    - Fixed spacing in experience/page.tsx React import: 'React,{' -> 'React, {'
    - Minor code style improvement
    - All tests and build continue to work correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import React from 'react';
 import { prof_projects } from '@/data/projects';
 import { getBlogPosts } from '@/data/blog';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
